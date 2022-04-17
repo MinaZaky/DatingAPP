5b8629d819b49eda8d88866b4e7f50fa16970174
Mina Gameel <Mina.Gameel@InteractTS.com>
2022-04-17 13:49:19 +0200

    End of section 3

--- API/Startup.cs
+++ API/Startup.cs
@@ -41,6 +41,7 @@ namespace API
                options.UseSqlServer(_config.GetConnectionString("DefaultConnection"));
             });
             services.AddControllers();
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -56,8 +57,8 @@ namespace API
             app.UseHttpsRedirection();
 
             app.UseRouting();
-
-            app.UseAuthorization();
+            app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
+            //app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
